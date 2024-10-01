8738d45601be947e19f4e4d94371a516e09d46d3
Anugra Wangchuk Lepcha <106028390+anugrawangchuk@users.noreply.github.com>
2024-10-01 13:54:49 +0530

    Update main.tf

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "bastion" {
 # Private EC2 Instance (Tool)
 resource "aws_instance" "private_ec2" {
   ami           = "ami-0a0e5d9c7acc336f1"
-  instance_type = "t2.micro"
+  instance_type = "t3.medium"
   key_name      = var.key_name
   subnet_id     = var.private_subnet_id
   security_groups = [var.private_sg_id] # Use security_group_ids here
